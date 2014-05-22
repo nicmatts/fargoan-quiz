b8b4d4969c6d4eecd6ec4f2c875cbfb7300b4c36
Nicole M <nicmatts@gmail.com>
2014-05-22 13:36:55 -0500

    add progress bar

--- app.js
+++ app.js
@@ -6,44 +6,47 @@ $(document).ready(function(){
 	var lakes = 0;
 	var weekend = 0;
 
-	$('button').on('click',function(){
-		windchill = Number($("input[name=windchill]:checked").val());
-		flood= Number($("input[name=flood]:checked").val());
-		lakes = Number($("input[name=lakes]:checked").val());
-		weekend = Number($("input[name=weekend]:checked").val());
-	});
-
-	var totalScore = windchill + flood + lakes + weekend;
-
 	$("#start-button").click(function(){
 		$("#start-button").hide();
 		$("#windchill").show(1000);
-		$('.progress').css({"width": "20px", "background-color": "blue"});
+		//$("#windchill").toggle("slide", { direction: "left" }, 1000);
+		$('.progress').css({"width": "20px"});
 	});
 
 	$("#windchill-button").click(function(){
 		$("#windchill").hide();
 		$("#flood").show(1000);
 		$('.progress').css("width", "40px");
+		windchill = Number($("input[name=windchill]:checked").val());
+		return windchill;
 	});
 
 	$("#flood-button").click(function(){
 		$("#flood").hide();
 		$("#lakes").show(1000);
 		$('.progress').css("width", "60px");
+		flood= Number($("input[name=flood]:checked").val());
+		return flood;
 	});
 
 	$("#lakes-button").click(function(){
 		$("#lakes").hide();
 		$("#weekend").show(1000);
 		$('.progress').css("width", "80px");
+		lakes = Number($("input[name=lakes]:checked").val());
+		return lakes;
 	});
 
 	$("#submit-button").click(function(){
 		$("#weekend").hide();
+		weekend = Number($("input[name=weekend]:checked").val());
+
+		var totalScore = windchill + flood + lakes + weekend;
+
+		//alert(totalScore);
 
 		if (totalScore <= 6){
-			$('#fargoan').show();
+			$("#fargoan").show();
 		} else if (totalScore >= 6 && totalScore <= 11 ){
 			$('#transplant').show();
 		} else if (totalScore == 12){
@@ -51,7 +54,12 @@ $(document).ready(function(){
 		} else {
 			$('#not-fargoan').show();
 		}
-		
+
+		$("#startover").show();
+	});
+
+	$('#reload').click(function() {
+		location.reload();
 	});
 });
 
